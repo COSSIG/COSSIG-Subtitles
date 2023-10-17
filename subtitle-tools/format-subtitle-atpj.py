ad0336bec76b1a1a4a4699b49313e748bd8fb161
Northurland <norlandtheartstudent@outlook.com>
2023-10-17 14:35:55 +0800

    创建 arctime 工程文件用的字幕格式化脚本；标注现阶段功能需求

---
+++ subtitle-tools/format-subtitle-atpj.py
@@ -0,0 +1,12 @@
+import urllib.parse as urlparse
+import json
+
+# TODO:
+# properly replace Chinese commas and full stops with spaces
+# properly remove spaces at the beginnings and endings of a sentence
+# properly replace “——” or “—” with " - "
+# Search and replace functionalities
+
+#  The atpj files use "+" to represent spaces
+#  which is different from what you get from urllib.parse function "quote"
+
